0adf3bcfe0915efaf2c3a0f3c4497b782acf3c2c
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-08-05 17:21:46 +0300

    feat: created next app

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const cors = require('cors')
-
 const Pusher = require("pusher");
 
 const pusher = new Pusher({
@@ -19,14 +18,14 @@ app.use(cors({
 
 app.use(express.json())
 
-app.post("/api/messages", async () => {
+app.post("/api/messages", async (req, res) => {
     await pusher.trigger("chat", "message", {
         username: req.body.username,
-        message:  req.body.message,
+        message: req.body.message,
     });
 
     res.json([]);
-})
+})  
 
 console.log('listening to port 8000')
 app.listen(8000)
